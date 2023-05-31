959432111853479cda3db72767d6c8199d890f7a
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-05-31 17:36:44 -0400

    GraphQL Pothos WIP

--- apps/epi-t3/src/pages/api/graphql.ts
+++ apps/epi-t3/src/pages/api/graphql.ts
@@ -1,9 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createSchema, createYoga } from "graphql-yoga";
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+import { /*createSchema,*/ createYoga } from "graphql-yoga";
 
-// Docs: https://vercel.com/docs/concepts/functions/serverless-functions
+import { prisma } from "@phac/db";
+
+// Pothos Docs: https://pothos-graphql.dev/docs/guide
 
 export const config = {
   api: {
@@ -12,22 +17,64 @@ export const config = {
   },
 };
 
+const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes;
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+    // defaults to false, uses /// comments from prisma schema as descriptions
+    // for object types, relations and exposed fields.
+    // descriptions can be omitted by setting description to false
+
+    // exposeDescriptions: boolean | { models: boolean, fields: boolean },
+    exposeDescriptions: { models: true, fields: true },
+
+    // use where clause from prismaRelatedConnection for totalCount (will true by default in next major version)
+    filterConnectionTotalCount: true,
+  },
+});
+
+builder.prismaObject("organizations", {
+  name: "organizations",
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    name: t.exposeString("name_en"),
+  }),
+});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (parent, { name }) => `hello, ${name || "World"}`,
+    }),
+    countOrgs: t.int({
+      description: "Name field",
+      resolve: async () => prisma.organizations.count(),
+    }),
+    orgnames: t.stringList({
+      description: "Name field",
+      resolve: async () =>
+        (await prisma.organizations.findMany()).map((o) => o.name_en),
+    }),
+    organizations: t.prismaField({
+      type: ["organizations"],
+      resolve: () => {
+        return prisma.organizations.findMany({});
+      },
+    }),
+  }),
+});
+
+const schema = builder.toSchema();
+
 export default createYoga<{
   req: NextApiRequest;
   res: NextApiResponse;
 }>({
   graphqlEndpoint: "/api/graphql",
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      type Query {
-        greetings: String
-      }
-    `,
-    resolvers: {
-      Query: {
-        greetings: () =>
-          "This is the `greetings` field of the root `Query` type",
-      },
-    },
-  }),
+  schema: schema,
 });
